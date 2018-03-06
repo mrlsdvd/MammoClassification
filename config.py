f825905ca3b504ec66cb9b5e7e333a4eb20719a9
mrlsdvd <mrlsdvd@DN0a1fcb65.SUNet>
2018-03-05 17:46:34 -0800

    Save numerical labels and one-hot encoded labels separately

--- config.py
+++ config.py
@@ -13,7 +13,9 @@ train_data_filename = 'mass_case_description_train_set.csv'
 test_data_filename = 'mass_case_description_test_set.csv'
 # processed data file names
 examples_filename = 'examples.csv'
+hot_labels_filename = 'hot_labels.csv'
 labels_filename = 'labels.csv'
+
 
 # Paths to data directories
 train_path = os.path.join(data_dir, train_dir)
@@ -27,22 +29,35 @@ train_roi_image_base = os.path.join(train_path, roi_image_dir)
 test_full_image_base = os.path.join(test_path, full_image_dir)
 test_roi_image_base = os.path.join(test_path, roi_image_dir)
 
-# Columns to extract
+# = Columns to extract =
 # List of column name(s) to use as features
 features = ['breast_density', 'abnormality id', 'mass shape', 'mass margins', 'subtlety']
 # List of column name to use as target (label)
 target = ['assessment']
 
+# = Model parameters =
+# Clustering parameters
+verbose = True # Display extra info?
+c = len(features) * 3
+m = 2 # Usually set to 2 as starting point
+maxiter = 300 # Based on sklearn k-means default
+error = 0.0001 # Based on sklearn k-means default
+seed = 142 # Seed for randomizer used by clusterer
+
 # Prediction directory paths and file names
 pred_dir = "predictions/"
+cluster_predictions_path = os.path.join(pred_dir, 'cluster_preds/')
+cluster_predictions_filename = 'predictions.txt'
 
 # Model directory paths and file names
 save_dir = 'models/'
+cluster_centers_path = os.path.join(save_dir, 'cluster_centers/')
+cluster_centers_filename = 'centers.txt'
 
 def setup():
 	# Create directories required
 	# Should only run once at beginning!!!!!!
-	directories = [train_processed_path, test_processed_path, pred_dir, save_dir]
+	directories = [train_processed_path, test_processed_path, cluster_predictions_path, cluster_centers_path, pred_dir, save_dir]
 	for directory in directories:
 		if not os.path.exists(directory):
 			os.makedirs(directory)
--- process_data.py
+++ process_data.py
@@ -9,11 +9,13 @@ def main(dataset):
     if dataset == 'train':
         dataset_filename = os.path.join(config.train_path, config.train_data_filename)
         processed_examples_filename = os.path.join(config.train_processed_path, config.examples_filename)
+        processed_hot_labels_filename = os.path.join(config.train_processed_path, config.hot_labels_filename)
         processed_labels_filename = os.path.join(config.train_processed_path, config.labels_filename)
     else:
         # Use test set
         dataset_filename = os.path.join(config.test_path, config.test_data_filename)
         processed_examples_filename = os.path.join(config.test_processed_path, config.examples_filename)
+        processed_hot_labels_filename = os.path.join(config.test_processed_path, config.hot_labels_filename)
         processed_labels_filename = os.path.join(config.test_processed_path, config.labels_filename)
 
     features = config.features
@@ -30,7 +32,8 @@ def main(dataset):
 
     # Save examples and labels to file
     one_hot_encoded_examples.to_csv(processed_examples_filename, header=True, index=False)
-    one_hot_encoded_labels.to_csv(processed_labels_filename, header=True, index=False)
+    one_hot_encoded_labels.to_csv(processed_hot_labels_filename, header=True, index=False)
+    labels.to_csv(processed_labels_filename, header=True, index=False)
 
 
 if __name__ == '__main__':
