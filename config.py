6da0190b0b5f9f54d149ed503fd708bfa6a3acf1
mrlsdvd <mrlsdvd@DN0a1fcb65.SUNet>
2018-03-10 22:41:55 -0800

    Add LDA model and Neural model

--- config.py
+++ config.py
@@ -15,6 +15,7 @@ test_data_filename = 'mass_case_description_test_set.csv'
 examples_filename = 'examples.csv'
 hot_labels_filename = 'hot_labels.csv'
 labels_filename = 'labels.csv'
+image_examples_filename = 'image_examples.csv'
 
 
 # Paths to data directories
@@ -35,6 +36,10 @@ features = ['breast_density', 'abnormality id', 'mass shape', 'mass margins', 's
 # List of column name to use as target (label)
 target = ['assessment']
 
+# Image size to apply
+# image_size = [5298, 3148] # Set to None to recompute during processing
+image_size = [2649, 1574] # Set to None to recompute during processing
+
 # = Model parameters =
 # Clustering parameters
 verbose = True # Display extra info?
@@ -44,15 +49,30 @@ maxiter = 300 # Based on sklearn k-means default
 error = 0.0001 # Based on sklearn k-means default
 seed = 142 # Seed for randomizer used by clusterer
 
+# Neural Network parameters
+num_hidden_neurons = 300
+output_size = 6
+batch_size = None
+epochs = 500
+
 # Prediction directory paths and file names
 pred_dir = "predictions/"
 cluster_predictions_path = os.path.join(pred_dir, 'cluster_preds/')
 cluster_predictions_filename = 'predictions.txt'
+lda_predictions_path = os.path.join(pred_dir, 'lda_preds/')
+lda_predictions_filename = 'predictions.txt'
+nn_predictions_path = os.path.join(pred_dir, 'neural_net_preds/')
+nn_predictions_filename = 'predictions.txt'
 
 # Model directory paths and file names
 save_dir = 'models/'
 cluster_centers_path = os.path.join(save_dir, 'cluster_centers/')
 cluster_centers_filename = 'centers.txt'
+lda_model_path = os.path.join(save_dir, 'lda/')
+lda_model_filename = 'model.pkl'
+nn_model_path = os.path.join(save_dir, 'neural_net/')
+nn_model_filename = 'nn.h5'
+
 
 def setup():
 	# Create directories required
--- models.py
+++ models.py
@@ -5,7 +5,30 @@ import logging
 import numpy as np
 import pandas as pd
 import skfuzzy as fuzz
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.externals import joblib
+from keras.models import Model
+from keras.layers import Dense, Input
+from keras import losses, metrics
+
+class LDA():
+    def __init__(self):
+        lda = LinearDiscriminantAnalysis()
+
+    def fit(self, data, labels):
+        self.lda.fit(data, labels)
+
+    def predict(self, data):
+        return lda.predict_proba(data)
+
+    def get_classes(self):
+        return self.lda.classes_
+
+    def save(self, model_path):
+        if not os.path.exists(os.path.dirname(model_path)):
+            os.makedirs(os.path.dirname(model_path))
+        joblib.dump(lda, model_path)
+
 
 class FuzzyCMEans():
     def __init__(self, c, m, maxiter, error, init=None, seed=None, verbose=True):
@@ -49,14 +72,50 @@ class FuzzyCMEans():
         self.centers = centers
 
 
+class Neural():
+    def __init__(self, input_size, num_hidden_neurons, output_size):
+        self.input_size = input_size
+        self.num_hidden_neurons = num_hidden_neurons
+        self.out_size = output_size
+        self.model = None
+
+    def build(self, weights_path=None):
+        x = Input(shape=(self.input_size, ))
+        h = Dense(self.num_hidden_neurons, activation='sigmoid')(x)
+        y = Dense(self.out_size, activation='softmax')(h)
+        self.model = Model(x, y)
+        self.model.compile(optimizer='Adam',
+                                    loss=losses.categorical_crossentropy,
+                                    metrics=[metrics.categorical_accuracy])
+        if weights_path:
+            self.model.load_weights(weights_path)
+
+    def save(self, model_path):
+        if not os.path.exists(os.path.dirname(model_path)):
+            os.makedirs(os.path.dirname(model_path))
+        self.model.save_weights(model_path)
+
+    def fit(self, examples, labels, batch_size, epochs):
+        self.model.fit(examples, labels, batch_size=batch_size, epochs=epochs)
+
+    def predict(self, examples):
+        predictions = self.model.predict(examples)
+        return predictions
+
+    def evaluate(self, examples, labels):
+        return self.model.evaluate(examples, labels)
+
+
 def main():
     # Debugging models and example use
     data_filename = os.path.join(config.train_processed_path, config.examples_filename)
     labels_filename = os.path.join(config.train_processed_path, config.labels_filename)
+    hot_labels_filename = os.path.join(config.train_processed_path, config.hot_labels_filename)
     data = pd.read_csv(data_filename, skiprows=0)
     labels = pd.read_csv(labels_filename, skiprows=0)
-    example = data.loc[5].reshape((1, data.loc[0].shape[0]))
-    label = labels.loc[5].reshape((1, labels.loc[0].shape[0]))
+    hot_labels = pd.read_csv(hot_labels_filename, skiprows=0)
+    # example = data.loc[13].reshape((1, data.loc[0].shape[0]))
+    # label = labels.loc[13].reshape((1, labels.loc[0].shape[0]))
     examples = data
 
     c = 10
@@ -69,11 +128,21 @@ def main():
     FCM.fit(data)
     FCM.save_centers(os.path.join(config.cluster_centers_path, config.cluster_centers_filename))
     predictions = FCM.predict(examples)
+    predictions = predictions.transpose()
     util.save_predictions(predictions, os.path.join(config.cluster_predictions_path, config.cluster_predictions_filename))
+    print predictions.shape
 
-    # QDA = QuadraticDiscriminantAnalysis()
-    # QDA.fit(examples, labels)
-    # print QDA.predict_proba(example)
+    # LDA = LinearDiscriminantAnalysis()
+    # LDA.fit(examples, labels)
+    # print LDA.classes_
+    # predictions = LDA.predict_proba(examples)
+    print predictions.shape
+    print hot_labels.shape
+
+    NN = Neural(predictions.shape[1], config.num_hidden_neurons, config.output_size)
+    NN.build()
+    NN.fit(predictions, hot_labels, config.batch_size, config.epochs)
+
 
 
 if __name__ == '__main__':
