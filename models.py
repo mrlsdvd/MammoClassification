9de12c54f8e581162a916d52e8aacc44af1d45b4
mrlsdvd <mrlsdvd@DN0a1fcb65.SUNet>
2018-03-11 17:10:39 -0700

    Implement correct soft clustering, instead of simple fuzzy clustering

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ import logging
 import numpy as np
 import pandas as pd
 import skfuzzy as fuzz
+from sklearn.utils import shuffle
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.externals import joblib
 from keras.models import Model
@@ -30,7 +31,7 @@ class LDA():
         joblib.dump(lda, model_path)
 
 
-class FuzzyCMEans():
+class SoftCluster():
     def __init__(self, c, m, maxiter, error, init=None, seed=None, verbose=True):
         logging.basicConfig(level=logging.INFO)
         self.logger = logging.getLogger(__name__)
@@ -41,32 +42,47 @@ class FuzzyCMEans():
         self.error = error
         self.init = init
         self.seed = seed
-        self.centers = None
-        self.U = None
+        self.centers = dict() # centers map (label -> center)
+        self.Us = dict() # Us map (label -> U)
 
-    def fit(self, data):
-        # fuzz.cluster.cmeans expects data to be of shape (# features, # examples)
-        data = data.transpose()
-        centers, U, _, _, _, p, fpc = fuzz.cluster.cmeans(data, c=self.c, m=self.m, error=self.error, maxiter=self.maxiter, init=self.init, seed=self.seed)
-        if self.verbose:
-            self.logger.info("Took {} iterations".format(p))
-            self.logger.info("fpc: {}".format(fpc))
-        self.centers = centers
-        self.U = U
+    def fit(self, data, labels):
+        # For each label, create fuzzy clusters
+        unique_labels = labels[config.target].values.flatten().tolist()
+        unique_labels = set(unique_labels)
+        unique_labels = sorted(unique_labels) # Sort to keep same order always
+        for label in unique_labels:
+            print label
+            # Get rows of current label
+            label_data = data.loc[labels.index[(labels[config.target[0]] == label)].tolist()]
+            # fuzz.cluster.cmeans expects data to be of shape (# features, # examples)
+            label_data = label_data.transpose()
+            # Compute soft clusters for label
+            centers, U, _, _, _, p, fpc = fuzz.cluster.cmeans(label_data, c=self.c, m=self.m, error=self.error, maxiter=self.maxiter, init=self.init, seed=self.seed)
+            if self.verbose:
+                self.logger.info("Took {} iterations".format(p))
+                self.logger.info("fpc: {}".format(fpc))
+            self.centers[label] = centers
+            self.Us[label] = U
 
     def predict(self, examples):
+        # Compute example fit in all soft clusters
         examples = examples.transpose()
         if self.centers is None:
             self.logger.error("Be sure to fit data or set centers, before attempting to predict!")
             return
-        U, _, _, _, _, _ = fuzz.cluster.cmeans_predict(examples, cntr_trained=self.centers, m=self.m, error=self.error, maxiter=self.maxiter, init=self.init, seed=self.seed)
-        return U
+        Us = []
+        for label in sorted(self.centers.keys()):
+            centers = self.centers[label]
+            U, _, _, _, _, _ = fuzz.cluster.cmeans_predict(examples, cntr_trained=centers, m=self.m, error=self.error, maxiter=self.maxiter, init=self.init, seed=self.seed)
+            Us.append(U.T)
+        return np.hstack(Us)
 
     def save_centers(self, filepath):
         if self.centers is None:
             self.logger.error("Be sure to fit data or set centers, before attempting to save!")
             return
-        np.savetxt(filepath, self.centers)
+        for label in sorted(self.centers.keys()):
+            np.savetxt("{}_{}.txt".format(filepath, label), self.centers[label])
 
     def set_centers(self, centers):
         self.centers = centers
@@ -87,7 +103,7 @@ class Neural():
         self.model.compile(optimizer='Adam',
                                     loss=losses.categorical_crossentropy,
                                     metrics=[metrics.categorical_accuracy])
-        if weights_path:
+        if weights_path != None:
             self.model.load_weights(weights_path)
 
     def save(self, model_path):
@@ -109,28 +125,37 @@ class Neural():
 def main():
     # Debugging models and example use
     data_filename = os.path.join(config.train_processed_path, config.examples_filename)
+    images_filename = os.path.join(config.train_processed_path, "image_examples.csv")
+    blobs_filename = os.path.join(config.train_processed_path, "blob_examples.csv")
     labels_filename = os.path.join(config.train_processed_path, config.labels_filename)
     hot_labels_filename = os.path.join(config.train_processed_path, config.hot_labels_filename)
     data = pd.read_csv(data_filename, skiprows=0)
+    print data.shape
+    images = pd.read_csv(images_filename, skiprows=0)
+    print images.shape
+    blobs = pd.read_csv(blobs_filename, skiprows=0, usecols=range(1, 4))
+    print blobs.shape
     labels = pd.read_csv(labels_filename, skiprows=0)
     hot_labels = pd.read_csv(hot_labels_filename, skiprows=0)
     # example = data.loc[13].reshape((1, data.loc[0].shape[0]))
     # label = labels.loc[13].reshape((1, labels.loc[0].shape[0]))
-    examples = data
+    examples = pd.concat([data, images, blobs], axis=1)
 
-    c = 10
-    m = 5
+    print examples.shape
+
+    c = 4
+    m = 2
     maxiter = 300 # Based on sklearn k-means default
     error = 0.0001 # Based on sklearn k-means default
     seed = 142 # Seed for randomizer used by clusterer
 
-    FCM = FuzzyCMEans(c, m, maxiter, error, init=None, seed=seed)
-    FCM.fit(data)
+    FCM = SoftCluster(c, m, maxiter, error, init=None, seed=seed)
+    FCM.fit(examples, labels)
     FCM.save_centers(os.path.join(config.cluster_centers_path, config.cluster_centers_filename))
     predictions = FCM.predict(examples)
-    predictions = predictions.transpose()
+    # predictions = np.log(predictions)
     util.save_predictions(predictions, os.path.join(config.cluster_predictions_path, config.cluster_predictions_filename))
-    print predictions.shape
+    print predictions
 
     # LDA = LinearDiscriminantAnalysis()
     # LDA.fit(examples, labels)
@@ -141,7 +166,8 @@ def main():
 
     NN = Neural(predictions.shape[1], config.num_hidden_neurons, config.output_size)
     NN.build()
-    NN.fit(predictions, hot_labels, config.batch_size, config.epochs)
+    shuffled_pred, shuffled_labels = shuffle(predictions, hot_labels.as_matrix(), random_state=142)
+    NN.fit(shuffled_pred, shuffled_labels, config.batch_size, config.epochs)
 
 
 
